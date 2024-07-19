23afa7389ce16c6f7ad8dec9ccb5229cea310d1f
AditRizkii <adityarizkiramadhan3@gmail.com>
2024-07-20 01:40:49 +0700

    upload-image feature

--- backend/routes/CaseRoute.js
+++ backend/routes/CaseRoute.js
@@ -1,12 +1,18 @@
 import express from "express";
-import { createCase, getCases, getCaseById, updateCase, deleteCase } from "../controllers/CaseController.js";
+import {
+  createCase,
+  getCases,
+  getCaseById,
+  updateCase,
+  deleteCase,
+} from "../controllers/CaseController.js";
 
-const router = express.Router();
+const Caserouter = express.Router();
 
-router.post("/admin-cases", createCase);
-router.get("/admin-cases", getCases);
-router.get("/admin-cases/:id", getCaseById);
-router.patch("/admin-cases/:id", updateCase);
-router.delete("/admin-cases/:id", deleteCase);
+Caserouter.post("/admin-cases", createCase);
+Caserouter.get("/admin-cases", getCases);
+Caserouter.get("/admin-cases/:id", getCaseById);
+Caserouter.patch("/admin-cases/:id", updateCase);
+Caserouter.delete("/admin-cases/:id", deleteCase);
 
-export default router;
+export default Caserouter;
